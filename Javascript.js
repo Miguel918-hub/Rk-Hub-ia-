ccd62c2c742dc8f63ec4d9256a9aa72e887aa867
Miguel918-hub <migueldiscord970@gmail.com>
2025-09-23 21:42:53 -0300

    Delete Javascript.js

--- Javascript.js
+++
@@ -1,4 +0,0 @@
-const scripts = [
-    { name: "Meu Script 1", description: "Descrição do meu primeiro script.", loadstring: "loadstring(game:HttpGet('...'))" },
-    { name: "Outro Script", description: "Este é outro script incrível.", loadstring: "loadstring(game:HttpGet('...'))" },
-];
